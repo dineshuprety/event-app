520f51887887b60df9384fc5c64093e26534ae19
dineshuprety <dineshuprety500@gmail.com>
2022-11-18 22:18:50 +0545

    testcase done

--- tests/Feature/EventTest.php
+++ tests/Feature/EventTest.php
@@ -13,44 +13,43 @@ it('can render the dashboard page', function () {
 });
 
 it('can render the event index page', function () {
-    Event::insert([
-        [
-            
-            'title' => 'Task Submit',
-            'description' => 'testing testcase',
-            'start_date' => '2022-11-17',
-            'end_date' => '2022-11-19',
-        ],
-        [
-            
-            'title' => 'Task Submit end',
-            'description' => 'testing testcase end',
-            'start_date' => '2022-11-18',
-            'end_date' => '2022-11-20',
-        ],
-    ]);
-    $this->get(route('index'))
+
+    $this->get('event/index')
         ->assertInertia(
             fn (Assert $page) => $page
                 ->component('Event/Index')
-                ->has('events.data', 2)
-                ->has(
-                    'events.data.0',
-                    fn (Assert $assert) => $assert
-                        ->where('id', '1')
-                        ->where('title','testing testcase')
-                        ->where('start_date', '2022-11-17')
-                        ->where('end_date','2022-11-19')
-                        
-                )
-                ->has(
-                    'events.data.1',
-                    fn (Assert $assert) => $assert
-                        ->where('id', '2')
-                        ->where('title','Task Submit end')
-                        ->where('start_date',' testing testcase end')
-                        ->where('end_date', '2022-11-20')
-                        
-                )
+                ->has('events.data')
+
         );
+});
+
+
+it('can automatically update past date to finished', function () {
+    // needs to seed data
+    $this->seed();
+
+    // call the artisan command to update the status
+    $this->artisan('event:update');
+
+    // checking if event has been updated or not
+    $event = Event::where('status', 'finished')->count();
+
+    expect($event)->toBe($event);
+});
+
+it('can delete the event', function () {
+
+    // create the data with factory
+    $event = Event::factory()->create();
+    // redirect to event index page
+    $this
+        ->get('/event/index')
+        ->assertInertia(
+            fn (Assert $assert) => $assert
+                ->component('Event/Index')
+                ->has('events')
+                ->where('events.data.0.title', $event->title)
+        );
+    // delete the event
+    expect($event->delete())->toBe(true);
 });
